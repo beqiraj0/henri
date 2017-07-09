e9ae5d1da25bd64d166069017869ec3c4df6cd19
beqiraj0 <beqiraj@icloud.com>
2017-07-09 20:08:51 +0200

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
-Product_ID = ""
+Security_Profile_Description ="henri"
+Security_Profile_ID  = "amzn1.application.0f4470d1c49444b28efcde57cb3f9a59"
+Client_ID = "amzn1.application-oa2-client.7a9a5b10b2b1494dad316180b4da87bd"
+Client_Secret = "7cd3143bfd64d18858be03e52c04d2fad9c6d01ec748f71a75ff6585632aa12d"
+Product_ID = "Henri"
 
 #Redis
-redis_url = ""
+redis_url = "redis://h:p20dedd1bbf3cb67c9cfbf70f98475e9d86e25cd1c5c05a590ec5c4b59975abbc@ec2-34-230-117-175.compute-1.amazonaws.com:18939"
